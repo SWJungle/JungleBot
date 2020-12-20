f9ccbe49fd1ac33ec6fac3cba3170f3cc4188da5
Sunio <sunio00000@naver.com>
2020-12-20 23:50:45 +0900

    Create slack_bot_run.sh

---
+++ slack_bot_run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#!/usr/bin/env python3
+PATH=/usr/local/bin:/usr/bin:/usr/local/sbin:/usr/sbin
+
+source /Users/eunjin/opt/anaconda3/etc/profile.d/conda.sh
+conda activate crawling_env
+python3 /Users/eunjin/Documents/slack_iphone11_bot.py
